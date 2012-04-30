d29a97ed4b674592827b06bdb9eedf09b3448e5b
Bradford Powell <bradford.powell@gmail.com>
2012-04-29 23:06:27 -0500

    initial import

---
+++ src/org/drpowell/grandannotator/GrandAnnotator.java
@@ -0,0 +1,95 @@
+package org.drpowell.grandannotator;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+import org.broad.tribble.readers.TabixReader;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.ParseException;
+
+public class GrandAnnotator {
+	private ArrayList<TabixVCFAnnotator> annotators;
+	
+	public GrandAnnotator(Reader jsonConfig) throws IOException, ParseException {
+		annotators = readConfigFromJSON(jsonConfig);
+	}
+
+	public void readConfigFromJS(String s) throws ScriptException {
+		ScriptEngineManager sem = new ScriptEngineManager();
+		ScriptEngine engine = sem.getEngineByName("JavaScript");
+		engine.eval(s);
+		Object config = engine.get("config");
+	}
+	
+	private ArrayList<TabixVCFAnnotator> readConfigFromJSON(Reader jsonReader) throws IOException, ParseException {
+		ArrayList<TabixVCFAnnotator> annotators = new ArrayList<TabixVCFAnnotator>();
+		JSONObject config = (JSONObject) JSONValue.parseWithException(jsonReader);
+		for (Map.Entry<String, Map> configEntry : ((Map<String, Map>) config).entrySet()) {
+			annotators.add(createAnnotatorFromConfig(configEntry.getKey(), configEntry.getValue()));
+		}
+		return annotators;
+	}
+	
+	private TabixVCFAnnotator createAnnotatorFromConfig(String tabixFile, Map config) throws IOException {
+		LinkedHashMap fields = new LinkedHashMap<String, String>();
+		if (config.containsKey("fields")) {
+			List unchangedFields = (List) config.get("fields");
+			for (Object s : unchangedFields) {
+				fields.put(s.toString(), s.toString());
+			}
+		}
+		if (config.containsKey("changedFields")) {
+			fields.putAll((Map) config.get("changedFields"));
+		}
+		TabixVCFAnnotator annotator = null;
+		annotator = new TabixVCFAnnotator(new TabixReader(tabixFile), fields);
+		if (config.containsKey("addChr")) {
+			annotator.setAddChr(true);
+		}
+		// TODO handle VCF filters, rsIDs, INFO headers
+		return annotator;
+	}
+
+	public void annotateVCFFile(BufferedReader input) throws IOException {
+		String line;
+		while ((line = input.readLine()) != null) {
+			if (line.startsWith("#")) {
+				System.out.println(line);
+			} else {
+				String [] row = line.split("\t");
+				String chromosome = row[0];
+				int start = Integer.parseInt(row[1]);
+				String ref = row[3];
+				String alt = row[4];
+				int end = start+ref.length()-1;
+				HashMap<String, String> info = TabixVCFAnnotator.splitInfoField(row[7]);
+				
+				for (TabixVCFAnnotator annotator: annotators) {
+					info = annotator.annotate(chromosome, start, end, ref, alt, info);
+				}
+				row[7] = TabixVCFAnnotator.joinInfo(info);
+				System.out.println(TabixVCFAnnotator.stringJoin("\t", row));
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		GrandAnnotator annotator = new GrandAnnotator(new BufferedReader(new FileReader(args[0])));
+		BufferedReader input = new BufferedReader(new FileReader(args[1]));
+		annotator.annotateVCFFile(input);
+	}
+
+
+}
---
+++ src/org/drpowell/grandannotator/TabixVCFAnnotator.java
@@ -0,0 +1,133 @@
+package org.drpowell.grandannotator;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.broad.tribble.readers.TabixIteratorLineReader;
+import org.broad.tribble.readers.TabixReader;
+
+public class TabixVCFAnnotator {
+	private final TabixReader tabix;
+	private final HashMap<String, String> fieldMap = new HashMap<String, String>();
+	private String prefix = ""; // can be "chr" if we need to add a prefix for query purposes
+
+	public static final String stringJoin(String delimiter, String[] strings) {
+		StringBuilder sb = new StringBuilder();
+		for (String string : strings) {
+			sb.append(string).append(delimiter);
+		}
+		return sb.substring(0, sb.length() - delimiter.length());
+	}
+			
+	public TabixVCFAnnotator(final TabixReader reader, final List<String> fields) {
+		tabix = reader;
+		for (String field : fields) {
+			fieldMap.put(field, field);
+		}
+	}
+	
+	public TabixVCFAnnotator(final TabixReader reader, final Map<String, String> fields) {
+		tabix = reader;
+		fieldMap.putAll(fields);
+	}
+	
+	public HashMap<String, String> annotate(final String chromosome, final int start, final int end, final String ref, final String alt, HashMap<String, String> info) {
+		Integer tid = tabix.mChr2tid.get(prefix + chromosome);
+		if (tid == null) {
+			// may want to log this...
+			return info;
+		}
+		String resultLine;
+		// when using this query form, tabix expects space-based (0-based) coordinates
+		TabixReader.Iterator iterator = tabix.query(tid, start-1, end);
+		if (iterator == null) {
+			return info;
+		}
+		try {
+			while ((resultLine = iterator.next()) != null) {
+				String [] targetRow = resultLine.split("\t");
+				// check on position (1), ref (3) and alt (4)
+				if (Integer.parseInt(targetRow[1]) == start && targetRow[3].equals(ref) && targetRow[4].equals(alt)) {
+					// found a match!
+					HashMap<String, String> targetInfo = splitInfoField(targetRow[7]);
+					for (Entry<String, String> e: fieldMap.entrySet()) {
+						if (targetInfo.containsKey(e.getKey())) {
+							// FIXME- should check to prevent duplicates being overwritten
+							info.put(e.getValue(), targetInfo.get(e.getKey()));
+						}
+					}
+					break;
+				}
+			}
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+		
+		return info;
+	}
+	
+	public String annotateLine(String [] row) {
+		String info = row[7];
+		HashMap<String, String> queryInfoMap = splitInfoField(info);
+		String query = prefix + row[0] + ":" + row[1] + "-" + row[1]; // I just search at the start position and check the ref,alt values
+		TabixIteratorLineReader lineReader = new TabixIteratorLineReader(tabix.query(query));
+		String line;
+		try {
+			while ((line = lineReader.readLine()) != null) {
+				String [] targetRow = line.split("\t");
+				// check on position (1), ref (3) and alt (4)
+				if (targetRow[1].equals(row[1]) && targetRow[3].equals(row[3]) && targetRow[4].equals(row[4])) {
+					// found a match!
+					HashMap<String, String> targetInfo = splitInfoField(targetRow[7]);
+					for (Entry<String, String> e: fieldMap.entrySet()) {
+						if (targetInfo.containsKey(e.getKey())) {
+							// FIXME- should check to prevent duplicates being overwritten
+							queryInfoMap.put(e.getValue(), targetInfo.get(e.getKey()));
+						}
+					}
+				}
+			}
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+		row[7] = joinInfo(queryInfoMap);
+		return stringJoin("\t", row);
+	}
+
+	public static String joinInfo(HashMap<String, String> info) {
+		StringBuilder sb = new StringBuilder();
+		for (Entry<String, String> e: info.entrySet()) {
+			sb.append(e.getKey()).append("=").append(e.getValue()).append(";");
+		}
+		return sb.substring(0, sb.length()-1); // no need for the last semicolon
+	}
+
+	public static HashMap<String, String> splitInfoField(String info) {
+		HashMap<String, String> map = new LinkedHashMap<String, String>();
+		String [] entries = info.split(";");
+		for (String entry : entries) {
+			String [] keyvalue = entry.split("=",2);
+			if (keyvalue.length == 1) {
+				// Flag field, has no value
+				String tmp = keyvalue[0];
+				keyvalue = new String[2];
+				keyvalue[0] = tmp;
+				keyvalue[1] = ""; // or could use null
+			}
+			if (map.containsKey(keyvalue[0])) {
+				throw new RuntimeException("Unable to deal with duplicated keys in the INFO field of a VCF");
+			}
+			map.put(keyvalue[0], keyvalue[1]);
+		}
+		return map;
+	}
+
+	public void setAddChr(boolean addChr) {
+		prefix = addChr? "chr" : "";
+	}
+
+}
