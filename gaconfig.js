0c9481c60a01d3e9135d9b9cd26978d31c61af80
Bradford Powell <bradford.powell@gmail.com>
2012-05-02 21:04:27 -0500

    change data file locations to the data/ directory
    need to figure out a good way to do relative paths (maybe relative to the config file?)

--- gaconfig.js
+++ gaconfig.js
@@ -1,12 +1,12 @@
-addVCFAnnotator("/home/bpow/anno/niehs/niehs.95samples.polymorphic.filtered.indels.vcf", "AF=NIEHSAF")
-addVCFAnnotator("/home/bpow/anno/niehs/niehs.95samples.polymorphic.filtered.snps.vcf", "AF=NIEHSAF")
-addVCFAnnotator("/home/bpow/anno/ALL.wgs.phase1_release_v3.20101123.snps_indels_sv.sites.vcf.gz",
+addVCFAnnotator("data/niehs.indels.vcf.gz", "AF=NIEHSAF")
+addVCFAnnotator("data/niehs.snps.vcf.gz", "AF=NIEHSAF")
+addVCFAnnotator("data/ALL.wgs.phase1_release_v3.20101123.snps_indels_sv.sites.vcf.gz",
     "AMR_AF,ASN_AF,AFR_AF,EUR_AF,AF=TGAF")
-addVCFAnnotator("/home/bpow/anno/dbsnp135ncbi.vcf.gz", 
+addVCFAnnotator("data/ncbiSNP135.vcf.gz", 
     "SCS,GMAF,CLN,PM,G5A,G5")
-addVCFAnnotator("/home/bpow/anno/ESP5400.vcf.gz",
+addVCFAnnotator("data/ESP5400.vcf.gz",
     "GTS,EA_GTC,AA_GTC,FG,GM,AA,AAC,PP,CDP,CG,GS,CA,DP=ESPDP,GL=ESPGL")
-addVCFAnnotator("/home/bpow/anno/Complete_Public_Genomes_54genomes_VQHIGH.vcf.gz",
+addVCFAnnotator("data/Complete_Public_Genomes_54genomes_VQHIGH.vcf.gz",
     "AF=CG54AF").setAddChr(true).setRequirePass(true)
-addTSVAnnotator("/home/bpow/anno/dbNSFP1.3/dbNSFP1.3.gz",
+addTSVAnnotator("data/dbNSFP1.3.gz",
     "17=PHYLOP,18=PHYLOP_PRED,19=SIFT,20=SIFT_PRED,21=PP2,22=PP2_PRED,23=LRT,24=LRT_PRED,25=MT,26=MT_PRED").checkRef(3).checkAlt(4)
