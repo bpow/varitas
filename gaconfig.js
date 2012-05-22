4b870834a638138e52b4ad84874ba9fcb7871e65
Bradford Powell <bradford.powell@gmail.com>
2012-05-22 13:56:36 -0500

    switched to 2.0b1 of dbNSFP

--- gaconfig.js
+++ gaconfig.js
@@ -8,5 +8,7 @@ addVCFAnnotator("data/ESP5400.vcf.gz",
     "MAF=ESPMAF,GTS,EA_GTC,AA_GTC,FG,GM,AA,AAC,PP,CDP,CG,GS,CA,DP=ESPDP,GL=ESPGL")
 addVCFAnnotator("data/Complete_Public_Genomes_54genomes_VQHIGH.vcf.gz",
     "AF=CG54AF").setAddChr(true).setRequirePass(true)
-addTSVAnnotator("data/dbNSFP1.3.gz",
-    "17=PHYLOP,18=PHYLOP_PRED,19=SIFT,20=SIFT_PRED,21=PP2,22=PP2_PRED,23=LRT,24=LRT_PRED,25=MT,26=MT_PRED").checkRef(3).checkAlt(4)
+addTSVAnnotator("data/dbNSFP2.0b1_variant.gz",
+    "22=SIFT,23=HDIV_SCORE,23=HDIV_PRED,25=HVAR_SCORE,26=HVAR_PRED," +
+    "27=LRT_SCORE,28=LRT_PRED,29=MT_SCORE,30=MT_PRED,31=GERP_NR,32=GERP_RS" +
+    "33=PHYLOP,36=LRT_OMEGA").checkRef(3).checkAlt(4)
