68cbd48d587851842c64897afb8c090db028f3be
Bradford Powell <bradford.powell@gmail.com>
2012-05-25 13:47:07 -0500

    VCF refactoring

--- src/org/drpowell/grandannotator/VCFFixedColumns.java
+++ src/org/drpowell/grandannotator/VCFFixedColumns.java
@@ -2,6 +2,6 @@ package org.drpowell.grandannotator;
 
 public enum VCFFixedColumns {
 	CHROM, POS, ID, REF, ALT, QUAL, FILTER, INFO, FORMAT;
-	private static final String VCF_FIXED_COLUMN_HEADER =
+	public static final String VCF_FIXED_COLUMN_HEADERS =
 			"#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT";
 }
