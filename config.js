ba6dffc90e2d1d6f32d4b0079f089c6c8722bc7a
Bradford Powell <bradford.powell@gmail.com>
2012-07-04 14:07:18 -0500

    updates to ESP/dbNSFP

--- config.js
+++ config.js
@@ -9,11 +9,11 @@ addVCFAnnotator("data/ALL.wgs.phase1_release_v3.20101123.snps_indels_sv.sites.vc
     "AMR_AF,ASN_AF,AFR_AF,EUR_AF,AF=TGAF")
 addVCFAnnotator("data/ncbiSNP135.vcf.gz", 
     "SCS,GMAF,CLN,PM,G5A,G5").setCopyID(true)
-addVCFAnnotator("data/ESP5400.vcf.gz",
+addVCFAnnotator("data/ESP6500.vcf.gz",
     "MAF=ESPMAF,GTS,EA_GTC,AA_GTC,FG,GM,AA,AAC,PP,CDP,CG,GS,CA,DP=ESPDP,GL=ESPGL")
 addVCFAnnotator("data/Complete_Public_Genomes_54genomes_VQHIGH.vcf.gz",
     "AF=CG54AF").setAddChr(true).setRequirePass(true)
-addTSVAnnotator("data/dbNSFP2.0b1_variant.gz",
+addTSVAnnotator("data/dbNSFP2.0b3_variant.gz",
     "22=SIFT,23=HDIV_SCORE,24=HDIV_PRED,25=HVAR_SCORE,26=HVAR_PRED," +
     "27=LRT_SCORE,28=LRT_PRED,29=MT_SCORE,30=MT_PRED,31=GERP_NR,32=GERP_RS," +
     "33=PHYLOP,36=LRT_OMEGA").checkRef(3).checkAlt(4)
