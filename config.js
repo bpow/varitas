ce4005478d0af4672abc1fbd806533aef8d4619c
Bradford Powell <bradford.powell@gmail.com>
2012-07-13 13:37:29 -0500

    added support for useHeader/hasHeader, hgnc in config.js

--- config.js
+++ config.js
@@ -3,7 +3,8 @@ addGeneAnnotator("in_omim", "data/omimgenes")
 addGeneAnnotator("in_hgmd", "data/hgmdgenes")
 addGeneAnnotator("in_cosmic", "data/cosmicgenes")
 addGeneAnnotator("exclusion", "data/fajardogenes")
-addVCFAnnotator("data/niehs95.indels.vcf.gz", "AF=NIEHSIAF")
+addGeneAnnotator("hgnclinks", "data/hgnc.txt").hasHeader(true).setKeyColumn(2).
+    setOutputColumns("3=HGNC_NAME")
 addVCFAnnotator("data/niehs95.snps.vcf.gz", "AF=NIEHSAF")
 addVCFAnnotator("data/ALL.wgs.phase1_release_v3.20101123.snps_indels_sv.sites.vcf.gz",
     "AMR_AF,ASN_AF,AFR_AF,EUR_AF,AF=TGAF")
@@ -16,4 +17,4 @@ addVCFAnnotator("data/Complete_Public_Genomes_54genomes_VQHIGH.vcf.gz",
 addTSVAnnotator("data/dbNSFP2.0b3_variant.gz",
     "22=SIFT,23=HDIV_SCORE,24=HDIV_PRED,25=HVAR_SCORE,26=HVAR_PRED," +
     "27=LRT_SCORE,28=LRT_PRED,29=MT_SCORE,30=MT_PRED,31=GERP_NR,32=GERP_RS," +
-    "33=PHYLOP,36=LRT_OMEGA").checkRef(3).checkAlt(4)
+    "33=PHYLOP,36=LRT_OMEGA").useHeader(true).checkRef(3).checkAlt(4)
