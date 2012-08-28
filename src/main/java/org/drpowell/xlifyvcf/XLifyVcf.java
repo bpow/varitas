220ea7f41c6b04639a3d98ee69322abdc4f1608d
Bradford Powell <bradford.powell@gmail.com>
2012-08-27 23:37:17 -0500

    clean imports

--- src/main/java/org/drpowell/xlifyvcf/XLifyVcf.java
+++ src/main/java/org/drpowell/xlifyvcf/XLifyVcf.java
@@ -7,13 +7,10 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.nio.file.AccessMode;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.BitSet;
-import java.util.EnumMap;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.zip.GZIPInputStream;
 
