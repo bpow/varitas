841a53a8cfed3b55cf3c73af85fa8ad6e5a132a0
Bradford Powell <bradford.powell@gmail.com>
2013-07-30 16:22:33 -0500

    support for additional headers in javascript filter

--- src/main/java/org/drpowell/vcffilters/ScriptVCFFilter.java
+++ src/main/java/org/drpowell/vcffilters/ScriptVCFFilter.java
@@ -1,18 +1,23 @@
 package org.drpowell.vcffilters;
 
 import java.io.Reader;
+import java.util.List;
+import java.util.logging.Logger;
 
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
+import org.drpowell.vcf.VCFHeaders;
 import org.drpowell.vcf.VCFIterator;
+import org.drpowell.vcf.VCFMeta;
 import org.drpowell.vcf.VCFVariant;
 
 public class ScriptVCFFilter extends VCFFilteringIterator {
 
 	private Invocable invocable;
+	private VCFHeaders headers;
 	
 	private Invocable initializeEngine(Reader script) {
 		ScriptEngineManager manager = new ScriptEngineManager();
@@ -24,11 +29,26 @@ public class ScriptVCFFilter extends VCFFilteringIterator {
 			System.err.println("Error preparing filter, will pass all variants!\n" + e);
 			engine = null;
 		}
+		Object moreHeaders = engine.get("headers");
+		if (moreHeaders != null) {
+			if (moreHeaders instanceof String) {
+				for (String header : ((String) moreHeaders).split("\n")) {
+					headers.add(new VCFMeta(header));
+				}
+			} else if (moreHeaders instanceof List) {
+				for (Object header : (List) moreHeaders) {
+					headers.add(new VCFMeta(header.toString()));
+				}
+			} else {
+				Logger.getLogger("VARITAS").warning("Unable to add headers for javascript filter");
+			}
+		}
 		return (Invocable) engine;
 	}
 
 	public ScriptVCFFilter(VCFIterator client, Reader fileReader) {
 		super(client);
+		headers = client.getHeaders();
 		invocable = initializeEngine(fileReader);
 	}
 
